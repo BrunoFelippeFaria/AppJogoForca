c8b9c97e6ce4669053b565d51dae2cf49c8a018b
bruno <brunofelippefaria@gmail.com>
2026-02-12 19:53:00 -0300

    feat: layout inicial

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -5,5 +5,28 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		GerarTeclado();
+	}
+
+	private void GerarTeclado()
+	{
+		string[] teclas = [
+			"Q", "W", "E", "R", "T", "Y", "U", "i", "O", "P",
+			"A", "S", "D", "F", "G", "H", "J", "k", "L", "Ç",
+			"Z", "X", "C", "V", "B", "N", "M"	
+		];
+
+		foreach (var tecla in teclas)
+		{
+			var btn = new Button
+			{
+				Text=tecla,
+				WidthRequest=20,
+				HeightRequest=20,
+			    Margin = new Thickness(0,0,3,3)
+			};
+
+			tecladoFlex.Add(btn);
+		}
 	}
 }
